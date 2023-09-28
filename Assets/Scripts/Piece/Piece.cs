228f5632cf337005ce64608446ab2830cb80de73
전해성 <seng19292@gmail.con>
2023-09-28 14:10:11 +0900

    no message

--- Assets/Scripts/Piece/Piece.cs
+++ Assets/Scripts/Piece/Piece.cs
@@ -191,6 +191,7 @@ public class Piece : MonoBehaviour
         GameObject box = Instantiate(randomBoxObject, transform.position, Quaternion.identity);
         RandomBox randomBox = box.GetComponent<RandomBox>();
         ArenaManager.Instance.fieldManagers[0].chest.CurveMove(randomBox.transform, fieldManager.targetPositions);
+        ArenaManager.Instance.fieldManagers[0].chest.SetBoxContents(randomBox, 0);
     }
 
     //¿Ãµø
