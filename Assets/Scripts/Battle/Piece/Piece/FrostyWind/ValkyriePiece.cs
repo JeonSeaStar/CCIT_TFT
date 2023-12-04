eb2cb0ef288a26b8b285ea0dcc05eda24f9a2996
dy990320 <dy990320@naver.com>
2023-12-04 16:59:39 +0900

    Feat: 이펙트 뭍히는거 고침

--- Assets/Scripts/Battle/Piece/Piece/FrostyWind/ValkyriePiece.cs
+++ Assets/Scripts/Battle/Piece/Piece/FrostyWind/ValkyriePiece.cs
@@ -30,7 +30,7 @@ public class ValkyriePiece : Piece
     {
         if (target != null)
         {
-            Instantiate(skillEffects, target.transform.position, Quaternion.identity);
+            Instantiate(skillEffects, new Vector3(target.transform.position.x, target.transform.position.y + 0.8f, target.transform.position.z), Quaternion.identity);
             Damage(damage);
         }
     }
