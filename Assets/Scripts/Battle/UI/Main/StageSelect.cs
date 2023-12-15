7679a2bf60539bc287d409b21c11b9d661083e5c
JeonSeastar <88014706+JeonSeaStar@users.noreply.github.com>
2023-12-15 22:00:22 +0900

    no message

--- Assets/Scripts/Battle/UI/Main/StageSelect.cs
+++ Assets/Scripts/Battle/UI/Main/StageSelect.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class StageSelect : MonoBehaviour
 {
+    public float radius = 2;
+    public List<GameObject> stageList;
+    public GameObject parent;
+
     public void StageSelectSwitch()
     {
         gameObject.SetActive(!gameObject.activeSelf);
@@ -13,5 +17,19 @@ public class StageSelect : MonoBehaviour
     {
         SoundManager.instance.Play("UI/Eff_Main_to_Ingame", SoundManager.Sound.Effect);
         SceneManagement.instance.SceneSwitching(false, "Battle");
+    }
+
+    private void CirclePlace()
+    {
+        int numOfChild = stageList.Count;
+
+        for (int i = 0; i < numOfChild; i++)
+        {
+            float angle = i * (Mathf.PI * 2.0f) / numOfChild;
+
+            GameObject child = stageList[i];
+
+            child.transform.position = parent.transform.position + (new Vector3(0, Mathf.Sin(angle), Mathf.Cos(angle))) * radius;
+        }
     }
 }
