b5fdf761573154ae8e705ffe059586b601afc787
Kang Seong Jun <dkdlfhqht789@naver.com>
2023-12-18 18:58:52 +0900

    Feat: 고양이 동전 연출 구현중

--- Assets/Scripts/Synerge/BuffData/Animal/Cat/CatCoin.cs
+++ Assets/Scripts/Synerge/BuffData/Animal/Cat/CatCoin.cs
@@ -3,9 +3,9 @@ using DG.Tweening;
 
 public class CatCoin : MonoBehaviour
 {
-    [SerializeField] float radius;
     [SerializeField] float moveSpeed;
-
+    [Header("¿Ãµø DoTween")] public Ease ease;
+    
     private void Start()
     {
         FieldManager.Instance.catcoin.Add(this);
@@ -15,14 +15,28 @@ public class CatCoin : MonoBehaviour
     {
         Vector3 targetPosition = FieldManager.Instance.readyTileList[2].transform.position;
         targetPosition.y += 0.5f;
-        transform.DOMove(targetPosition, moveSpeed);
+        transform.DOMove(targetPosition, moveSpeed).SetEase(ease);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "ReadyTile")
         {
-            Debug.Log(other.name);
+            //gameObject.AddComponent<RectTransform>();
+            //RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+            //rectTransform.anchorMin = Vector2.one;
+            //rectTransform.anchorMax = Vector2.one;
+            //gameObject.transform.parent = FieldManager.Instance.pieceShop.gameObject.transform.parent;
+
+            //transform.position = Vector3.MoveTowards(transform.position, FieldManager.Instance.pieceShop.text.transform.position, Time.deltaTime * 7.5f);
+            Vector3 goldPosition = Camera.main.WorldToScreenPoint(FieldManager.Instance.playerState.currentMoneyText.transform.position);
+            Vector3 goldPosition2 = Camera.main.ScreenToWorldPoint(goldPosition);
+            //goldPosition.z = 0;
+            //Debug.Log(goldPosition2);
+            //Vector3 translatePosition = Camera.main.ScreenToWorldPoint(goldPosition);
+            //Vector3 translatePosit2ion = Camera.main.ScreenToViewportPoint(goldPosition); Debug.Log(translatePosit2ion);
+            //transform.DOMove(translatePosition, 10f);
+            //transform.DOScale(Vector3.zero, 1f);
         }
     }
 }
